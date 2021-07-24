65f73b2524642cf9ee93d2ebdfc475308a0146b4
duohua28 <Duohua28@gmail.com>
2021-07-24 09:37:47 +0800

    master

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World!"
 
-app.run() 
+    a=10
+    b=6
+    c=0
+    result=(a+b)/c
+    return '<h1>Happy</h1>' % result
+
+app.run(debug=True) 

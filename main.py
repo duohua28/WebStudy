7b6f420546a072ea183639ed87363365dc828da6
duohua28 <Duohua28@gmail.com>
2021-07-25 09:40:29 +0800

    master

--- main.py
+++ main.py
@@ -5,12 +5,9 @@ app = Flask(__name__)
 def index():
 
     a=10
-
     b=6
-    
     c=0
-    
-    result=(a+b)/c
-    return '<h1>Happy</h1>' % result
+    # result=(a+b)/c
+    return '<h1>Happy</h1>'
 
-app.run(debug=True) 
+app.run(debug=True)

8f8f8764b1fb0fd61727fda8ef4014936d1ea1c0
duohua28 <Duohua28@gmail.com>
2021-07-26 11:28:33 +0800

    master

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
+from flask_script import Manager
+
 app = Flask(__name__)
+
+manager = Manager(app=app)
 
 @app.route("/",methods=['GET'])
 def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    manager.run()
